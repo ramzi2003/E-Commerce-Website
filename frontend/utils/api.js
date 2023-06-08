513f41d30e658e02c2d7bf33734dab6ba8e19557
Ramzi Shodmonbekov <ramzi.shodmonbekov_2026@ucentralasia.org>
2023-06-08 20:31:35 +0600

    final update

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,6 +1,6 @@
 import { API_URL, STRAPI_API_TOKEN } from "./urls";
 
-export const fetchDataFromApi = async () => {
+export const fetchDataFromApi = async (endpoint) => {
   const options = {
     method: "GET",
     headers: {
@@ -11,5 +11,18 @@ export const fetchDataFromApi = async () => {
   const res = await fetch(`${API_URL}${endpoint}`, options);
   const data = await res.json();
 
+  return data;
+};
+
+export const makePaymentRequest = async (endpoint, payload) => {
+  const res = await fetch(`${API_URL}${endpoint}`, {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + STRAPI_API_TOKEN,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+  const data = await res.json();
   return data;
 };
